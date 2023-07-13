ba28433a57a20ae130e395a5767e2d640a360cf6
ahmedmekni <121110303+ahmedmekni@users.noreply.github.com>
2023-07-13 12:58:12 +0100

    Add files via upload

---
+++ assci value .cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+int main()
+{
+
+int ascii;
+cout << "ascii value ? :";
+cin >> ascii;
+cout <<"characer is :" << char(ascii) << endl;
+
+char character;
+cout <<"character : ";
+cin >> character;
+cout << "ascii value is :" << int(character);
+
+
+
+}
